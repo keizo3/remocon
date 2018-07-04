6054141d2203922c8ce70ad19407d7f507a2e386
Keizo Suzuki <keizo.suzuki3@gmail.com>
2018-07-04 16:19:22 +0000

    add cooler

--- run.sh
+++ run.sh
@@ -7,9 +7,12 @@ REPEAT=1
 CASE_TV="テレビ"
 CASE_CHANNEL="チャンネル"
 CASE_VOLUME="ボリューム"
-CASE_AIRCON="エアコン"
+CASE_HEATER="暖房"
+CASE_COOLER="冷房"
 CASE_TEMP="温度"
 CASE_REBOOT="リブート"
+
+echo $1 >> /var/log/remoconlog.txt
 
 case $1 in
     ${CASE_TV}* )
@@ -21,9 +24,13 @@ case $1 in
     ${CASE_VOLUME}* )
         DIR="tv"
         ACTION=${1/${CASE_VOLUME}/""};;
-    ${CASE_AIRCON}* )
-        DIR="aircon"
-        ACTION=${1/${CASE_AIRCON}/""}
+    ${CASE_HEATER}* )
+        DIR="heater"
+        ACTION=${1/${CASE_HEATER}/""}
+        REPEAT=3;;
+    ${CASE_COOLER}* )
+        DIR="cooler"
+        ACTION=${1/${CASE_COOLER}/""}
         REPEAT=3;;
     ${CASE_TEMP}* )
         DIR="aircon"
@@ -31,8 +38,14 @@ case $1 in
         REPEAT=3;;
     ${CASE_REBOOT}* )
         sudo reboot
+        echo "reboot!!" >> /var/log/remoconlog.txt
+        exit;;
+    * )
+        sudo reboot
         exit;;
 esac
+
+echo "/home/pi/dev/remocon/${DIR}/${ACTION}.txt" >> /var/log/remoconlog.txt
 
 if [ ${DIR} != "" ] ; then
     for i in `seq 1 ${REPEAT}`
